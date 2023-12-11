54a003a258ac0de20497318ae474fe34aebb7acc
EzhilarasanElangovan31 <ezhilarasan.elangovan@syncfusion.com>
2023-12-11 11:59:09 +0530

    Updated sample

---
+++ Add benchmark line/Sample/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Model> Data { get; set; }
+
+        public ViewModel()
+        {
+            Data = new ObservableCollection<Model>()
+            {
+                new Model {  XValue = "Group A", YValue = 50 },
+                new Model {  XValue = "Group B", YValue = 60 },
+                new Model {  XValue = "Group C", YValue = 70 },
+                new Model {  XValue = "Group D", YValue = 40 }
+            };
+        }
+    }
+}
